e2bd21eaa458fe1770de1eed356205861bbae1b1
István Kurucz <kurucz.isti63@gmail.com>
2025-07-11 14:57:44 +0200

    User route simplified.

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,7 +1,5 @@
 import { Router } from "express";
 import authUserMW from "../middlewares/auth/auth/authUserMW";
-import validateCreateUserDataMW from "../middlewares/user/validateCreateUserDataMW";
-import createUserMW from "../middlewares/user/createUserMW";
 import returnUserMW from "../middlewares/user/returnUserMW";
 import validateUserIdMW from "../middlewares/user/validateUserIdMW";
 import getUserMW from "../middlewares/user/getUserMW";
@@ -9,7 +7,6 @@ import checkUserUpdateAccessMW from "../middlewares/user/checkUpdateUserAccessMW
 import validateUpdateUserDataMW from "../middlewares/user/validateUpdateUserDataMW";
 import updateUserMW from "../middlewares/user/updateUserMW";
 import checkDeleteUserAccessMW from "../middlewares/user/checkDeleteUserAccessMW";
-import deleteUserMW from "../middlewares/user/deleteUserMW";
 import deleteAuthUserMW from "../middlewares/user/deleteAuthUserMW";
 import sendUserDeletedResponseMW from "../middlewares/user/sendUserDeletedResponseMW";
 
@@ -18,8 +15,7 @@ const router = Router();
 // Add auth MWs
 router.use(authUserMW);
 
-// Create user
-router.post("/", validateCreateUserDataMW, createUserMW, returnUserMW);
+// User is added to DB automatically via Supabase trigger
 
 // Get user
 router.get("/:userId", validateUserIdMW, getUserMW, returnUserMW);
@@ -39,7 +35,6 @@ router.delete(
 	"/:userId",
 	validateUserIdMW,
 	checkDeleteUserAccessMW,
-	deleteUserMW,
 	deleteAuthUserMW,
 	sendUserDeletedResponseMW
 );
--- src/utils/user/validation/schemas/createUserSchema.ts
+++
@@ -1,9 +0,0 @@
-import z from "zod/v4";
-
-const createUserSchema = z.object({
-	id: z.uuid("Invalid user ID."),
-	name: z.string().trim().nonempty("Name is missing."),
-});
-
-export default createUserSchema;
-export type CreateUserData = z.infer<typeof createUserSchema>;
