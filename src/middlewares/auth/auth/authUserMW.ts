3b14cd5c5085aeb91772b55803049091a47e8d9d
István Kurucz <kurucz.isti63@gmail.com>
2025-07-13 12:33:21 +0200

    Auth MWs moved to other folder 2

--- src/middlewares/auth/auth/authUserMW.ts
+++
@@ -1,8 +0,0 @@
-import getAuthTokenMW from "./getAuthTokenMW";
-import getUserFromAuthMW from "./getUserFromAuthMW";
-import validateAuthorizationHeaderMW from "./validateAuthorizationHeaderMW";
-
-// Array of auth MWs
-const authUserMW = [validateAuthorizationHeaderMW, getAuthTokenMW, getUserFromAuthMW];
-
-export default authUserMW;
